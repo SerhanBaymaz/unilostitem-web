760a803e6b6c4b3f10dd70825f5d630f54ff14b7
Serhan Baymaz <serhanbaymaz1@gmail.com>
2026-05-06 22:23:50 +0300

    feat: integrate Header into MainLayout for proper page wrapping

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,13 @@
-import { Outlet } from 'react-router';
+import { Outlet } from "react-router";
+import { Header } from "@/shared/components";
 
 export default function MainLayout() {
-  return (
-    <div className="min-h-svh bg-background">
-      <Outlet />
-    </div>
-  );
+	return (
+		<div className="flex min-h-svh flex-col bg-background">
+			<Header />
+			<main className="flex-1">
+				<Outlet />
+			</main>
+		</div>
+	);
 }
